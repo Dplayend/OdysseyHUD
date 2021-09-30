0ec5f97a05e48492327793965f9d9312eff6f809
Dplayend <69760998+Dplayend@users.noreply.github.com>
2021-09-30 20:04:09 -0300

    Delete src/main directory

--- src/main/java/com/dplayend/odysseyhud/OdysseyHUD.java
+++
@@ -1,9 +0,0 @@
-package com.dplayend.odysseyhud;
-
-import net.fabricmc.api.ModInitializer;
-
-public class OdysseyHUD implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
